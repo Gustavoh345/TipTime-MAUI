d539fa4a3c8254179d33816b48c7cc167f4559d2
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2025-09-01 22:31:51 -0300

    Porcentagem de gorjeta

---
+++ TipTime/CalculoGorjeta.xaml.cs
@@ -0,0 +1,29 @@
+namespace TipTime;
+
+public partial class CalculoGorjeta : ContentPage
+{
+	public CalculoGorjeta()
+	{
+		InitializeComponent();
+	}
+
+    private void QuinzeBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void VinteBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void RoundDownBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void RoundUpBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ TipTime/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /*private void OnCounterClicked(object sender, EventArgs e)
+         {
+             count++;
+
+             if (count == 1)
+                 CounterBtn.Text = $"Clicked {count} time";
+             else
+                 CounterBtn.Text = $"Clicked {count} times";
+
+             SemanticScreenReader.Announce(CounterBtn.Text);
+         }*/
+
+        private void QuinzeBtn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 15;
+
+
+        }
+
+        private void VinteBtn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 20;
+        }
+
+        private void RoundDownBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundUpBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double ValorTotal = Double.Parse(ValorTotalEntry.Text);
+            double Porcentagem = PorcentagemSlider.Value/100;
+            double Gorjeta = PorcentagemSlider.Value * ValorTotal;
+            ValorGorjetaLabel.Text = Gorjeta + "%";
+            double ValorFinal = Gorjeta + ValorTotal;
+        }
+    }
+
+}
