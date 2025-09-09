e1b5e76bc9e846114ef0a24f763b22ce0706a916
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2025-09-08 22:33:04 -0300

    correcao da aula

--- TipTime/MainPage.xaml.cs
+++ TipTime/MainPage.xaml.cs
@@ -7,6 +7,8 @@
         public MainPage()
         {
             InitializeComponent();
+
+            PorcentagemSlider.Value = 17;
         }
 
         /*private void OnCounterClicked(object sender, EventArgs e)
@@ -45,11 +47,38 @@
 
         private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            double ValorTotal = Double.Parse(ValorTotalEntry.Text);
-            double Porcentagem = PorcentagemSlider.Value/100;
-            double Gorjeta = PorcentagemSlider.Value * ValorTotal;
-            ValorGorjetaLabel.Text = Gorjeta + "%";
-            double ValorFinal = Gorjeta + ValorTotal;
+            try
+            {
+                double ValorTotal = Convert.ToDouble(ValorTotalEntry.Text);
+                double Porcentagem = PorcentagemSlider.Value / 100;
+                /*PorcentagemGorjeta = $"{Porcentagem}";*/
+                double Gorjeta = Porcentagem * ValorTotal;
+                //ValorGorjetaLabel.Text = $"R$ {Gorjeta}";
+                ValorGorjetaLabel.Text = Gorjeta.ToString("c");
+                double ValorFinal = Gorjeta + ValorTotal;
+                ValorTotalLabel.Text = $"{ValorFinal}";
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString);
+                DisplayAlert("Erro.", "Por gentileza digite um valor valido.", "OK");
+            }
+
+
+
+
+            //Do professor Bellotti:
+            /*double porcentagem = PorcentagemSlider_Value;
+            double gorjeta;
+            double valortotal = double..Parse(ValorTotalEntry.Text);
+            //outra forma de converter double valortotal = Convert.ToDouble(ValorTotalEntry.Text);
+            float = valorfinal;
+            gorjeta = valortotal * (porcentagem / 100);
+            ValorGorjetaLabel.Text = $"R$ {gorjeta}";
+            //Estamos interpolando uma variavél que tem conteudo
+            double valorfinal = valortotoal + gorjeta;
+            ValorFinalLabel.Text = $"R$ {valorfinal}"*/
         }
     }
 
